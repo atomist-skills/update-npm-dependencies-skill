28566fa83078d4afef3d3b4f6bdf579c025a3f95
Jim Clark <slimslenderslacks@gmail.com>
2020-03-09 18:37:31 -0700

    upgrade api-cljs

--- dev/user.cljs
+++ dev/user.cljs
@@ -5,7 +5,10 @@
 (enable-console-print!)
 
 (def token (.. js/process -env -API_KEY_SLIMSLENDERSLACKS_STAGING))
+#_(def token (.. js/process -env -API_KEY_SLIMSLENDERSLACKS_PROD))
 (def github-token (.. js/process -env -GITHUB_TOKEN))
+
+(def team-id "APGF6BRLR")
 
 (comment
  ;; this should fail with a bad JSON
@@ -24,14 +27,27 @@
  ;; this should succeed
  (atomist.main/handler #js {:data {:Push [{:branch "master"
                                            :repo {:name "express-test-0"
-                                                  :org {:owner "atomisthqa"
+                                                  :org {:owner "slimslender"
                                                         :scmProvider {:providerId "zjlmxjzwhurspem"
                                                                       :credential {:secret github-token}}}}
                                            :after {:message ""}}]}
                             :secrets [{:uri "atomist://api-key" :value token}]
                             :configurations [{:parameters [{:name "policy" :value "manualConfiguration"}
                                                            {:name "dependencies" :value "{\"express\": \"4.1.2\"}"}]}]
-                            :extensions {:team_id "AK748NQC5"}}
+                            :extensions {:team_id team-id}}
+                       (fn [& args] (log/info "sendreponse " args)))
+
+ ;; running in a workspace linked to slimslender
+ (atomist.main/handler #js {:data {:Push [{:branch "master"
+                                           :repo {:name "npm1-test"
+                                                  :org {:owner "slimslender"
+                                                        :scmProvider {:providerId "zjlmxjzwhurspem"
+                                                                      :credential {:secret github-token}}}}
+                                           :after {:message ""}}]}
+                            :secrets [{:uri "atomist://api-key" :value token}]
+                            :configurations [{:parameters [{:name "policy" :value "manualConfiguration"}
+                                                           {:name "dependencies" :value "{\"@slimslender/npm-test\": \"0.4.139\"}"}]}]
+                            :extensions {:team_id team-id}}
                        (fn [& args] (log/info "sendreponse " args)))
 
 
