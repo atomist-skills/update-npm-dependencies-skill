6daf2cd5dd53657f6e331f84405aac6f3ba58685
Jim Clark <slimslenderslacks@gmail.com>
2020-05-06 20:42:50 -0700

    only do pushes on master

--- dev/user.cljs
+++ dev/user.cljs
@@ -1,61 +1,33 @@
 (ns user
   (:require [atomist.main]
+            [atomist.local-runner :refer [set-env call-event-handler fake-push fake-command-handler]]
             [atomist.cljs-log :as log]))
 
 (enable-console-print!)
-
-(def token (.. js/process -env -API_KEY_SLIMSLENDERSLACKS_STAGING))
-#_(def token (.. js/process -env -API_KEY_SLIMSLENDERSLACKS_PROD))
-(def github-token (.. js/process -env -GITHUB_TOKEN))
-
-(def team-id "APGF6BRLR")
+(set-env :prod-github-auth)
 
 (comment
  ;; this should fail with a bad JSON
-  (atomist.main/handler #js {:data {:Push [{:branch "master"
-                                            :repo {:name "express-test-0"
-                                                   :org {:owner "atomisthqa"
-                                                         :scmProvider {:providerId "zjlmxjzwhurspem"
-                                                                       :credential {:secret github-token}}}}
-                                            :after {:message ""}}]}
-                             :secrets [{:uri "atomist://api-key" :value token}]
-                             :configurations [{:parameters [{:name "policy" :value "manualConfiguration"}
-                                                            {:name "dependencies" :value "{express \"4.1.2\"}"}]}]
-                             :extensions {:team_id "AK748NQC5"}}
-                        (fn [& args] (log/info "sendreponse " args)))
+ (-> (fake-push "AK748NQC5" "atomisthqa" "express-test-0" "master")
+     (assoc :configurations [{:parameters [{:name "policy" :value "manualConfiguration"}
+                                           {:name "dependencies" :value "{express \"4.1.2\"}"}]}])
+     (call-event-handler atomist.main/handler))
 
- ;; this should succeed
-  (atomist.main/handler #js {:data {:Push [{:branch "master"
-                                            :repo {:name "express-test-0"
-                                                   :org {:owner "slimslender"
-                                                         :scmProvider {:providerId "zjlmxjzwhurspem"
-                                                                       :credential {:secret github-token}}}}
-                                            :after {:message ""}}]}
-                             :secrets [{:uri "atomist://api-key" :value token}]
-                             :configurations [{:parameters [{:name "policy" :value "manualConfiguration"}
-                                                            {:name "dependencies" :value "{\"express\": \"4.1.2\"}"}]}]
-                             :extensions {:team_id team-id}}
-                        (fn [& args] (log/info "sendreponse " args)))
+ (-> (fake-command-handler "AK748NQC5" "UpdateNpmDependency" "npm update" "CUCEERLBH" "UDF0NFB5M")
+     (assoc :parameters [{:name "dependency"
+                          :value "{\"express\": \"4.1.2\"}"}])
+     (call-event-handler atomist.main/handler))
 
- ;; running in a workspace linked to slimslender
-  (atomist.main/handler #js {:data {:Push [{:branch "master"
-                                            :repo {:name "npm1-test"
-                                                   :org {:owner "slimslender"
-                                                         :scmProvider {:providerId "zjlmxjzwhurspem"
-                                                                       :credential {:secret github-token}}}}
-                                            :after {:message ""}}]}
-                             :secrets [{:uri "atomist://api-key" :value token}]
-                             :configurations [{:parameters [{:name "policy" :value "manualConfiguration"}
-                                                            {:name "dependencies" :value "{\"@slimslender/npm-test\": \"0.4.139\"}"}]}]
-                             :extensions {:team_id team-id}}
-                        (fn [& args] (log/info "sendreponse " args)))
+ (-> (fake-command-handler "T29E48P34" "ShowNpmDependencies" "npm fingerprints" "C012WB9MCCB" "U2ATJPCSK")
+     (assoc :configurations [{:name "test"
+                              :enabled true
+                              :parameters [{:name "policy" :value "manualConfiguration"}
+                                           {:name "dependencies" :value "{\"@google-cloud/pubsub\": \"1.7.2\"}"}]}])
+     (call-event-handler atomist.main/handler))
 
-  (atomist.main/handler #js {:command "UpdateNpmDependency"
-                             :source {:slack {:channel {:id "CUCEERLBH"}
-                                              :user {:id "UDF0NFB5M"}}}
-                             :team {:id "AK748NQC5"}
-                             :parameters [{:name "dependency"
-                                           :value "{\"express\": \"4.1.2\"}"}]
-                             :raw_message "npm update"
-                             :secrets [{:uri "atomist://api-key" :value token}]}
-                        (fn [& args] (log/info "sendreponse " args))))
+ (-> (fake-command-handler "T29E48P34" "SyncNpmDependency" "npm sync" "C012WB9MCCB" "U2ATJPCSK")
+     (assoc :configurations [{:name "test"
+                              :enabled true
+                              :parameters [{:name "policy" :value "manualConfiguration"}
+                                           {:name "dependencies" :value "{\"@google-cloud/pubsub\": \"1.7.2\"}"}]}])
+     (call-event-handler atomist.main/handler)))
