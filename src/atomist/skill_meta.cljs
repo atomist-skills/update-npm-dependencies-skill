ced236ee444ef925eaf44b906e6c6f866473b9b1
Jim Clark <slimslenderslacks@gmail.com>
2020-07-29 12:08:22 -0700

    Bump update-npm-dependencies-skill to 0.3.3 and spit metadata

--- src/atomist/skill_meta.cljs
+++ src/atomist/skill_meta.cljs
@@ -3,6 +3,6 @@
 
 (def module-name "update-npm-dependencies-skill")
 (def path ".")
-(def version "0.3.2")
-(def tag "v0.3.2")
-(def generated-at "2020-05-15T23:51:25Z")
+(def version "0.3.3")
+(def tag "0.3.3")
+(def generated-at "2020-07-29T19:08:22Z")
